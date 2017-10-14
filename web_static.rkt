c6c20c4bfd9ffa1e06bbdc0c6a6e6c75e2b3d7fc
gisi <7enderhead@gmail.com>
2017-10-14 12:55:48 +0200

    selection values

--- web_static.rkt
+++ web_static.rkt
@@ -1,14 +1,23 @@
 #lang web-server/insta
 
+(require racket/format)
+(require web-server/formlets)
 (require "db_test.rkt")
 
 (define (person-list-items)
   (for/list ([person (all-persons)])
-    `(option ,(vector-ref person 1))))
+    `(option ([value ,(~a (vector-ref person 0))])
+             ,(vector-ref person 1))))
 
-(define (start request)
+(define (render-persons [selected-person #f])
   (response/xexpr
    `(html
      (head (title "Static Web Page"))
      (body (h1 "List of Persons")
-           (select ([size "5"]) ,@(person-list-items))))))
+           (form 
+            (select ([size "5"]
+                    [name "person"])
+                   ,@(person-list-items)))))))
+
+(define (start request)
+  (render-persons))
