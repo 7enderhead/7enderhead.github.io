8844a0aca4f92676cd32d0220bbec9529f179f33
7enderhead <7enderhead@gmail.com>
2017-11-09 17:18:56 +0100

    starting compound-stop-selector

---
+++ sub2/compound-stop-selector.rkt
@@ -0,0 +1,292 @@
+#lang racket
+
+(require racket/gui/base)
+(require racket/struct)
+(require anaphoric)
+(require threading)
+(require (rename-in "data-defs.rkt"
+                    (min-lon def:min-lon)
+                    (max-lon def:max-lon)
+                    (min-lat def:min-lat)
+                    (max-lat def:max-lat)))
+(require "data-list-box.rkt")
+(require "slider-converter.rkt")
+(require "util.rkt")
+
+(struct list-layout (filter-expr min-lon max-lon min-lat max-lat sorting)
+  #:transparent
+  #:methods gen:custom-write
+  [(define write-proc
+     (make-constructor-style-printer
+      (lambda (l) 'list-layout)
+      (lambda (l) (list (list-layout-filter-expr l)
+                        (list-layout-min-lon l)
+                        (list-layout-max-lon l)
+                        (list-layout-min-lat l)
+                        (list-layout-max-lat l)
+                        (list-layout-sorting l)))))])
+
+(define column-mappings #(("Name" name)
+                          ("Longitude" lon-range)
+                          ("Latitude" lat-range)))
+
+(define column-names
+  (map (match-lambda [(list name accessor) name])
+       (vector->list column-mappings)))
+
+(define compound-stop-selector%
+  (class object%
+
+    (init stops parent [selection-id #f] [callback #f] [focus #f])
+
+    (super-new)
+
+    ;;; internal fields
+    
+    (define selected-stop #f)
+    (define list-sorting 0)
+
+    (define min-lon (def:min-lon stops))
+    (define max-lon (def:max-lon stops))
+    (define min-lat (def:min-lat stops))
+    (define max-lat (def:max-lat stops))
+    
+    (define slider-converter
+      (new slider-converter%
+           [min-lon min-lon]
+           [max-lon max-lon]
+           [min-lat min-lat]
+           [max-lat max-lat]))
+      
+    ;;; control fields
+    
+    (define panel (new vertical-panel%
+                       [parent parent]))
+
+    (define selection-panel (new horizontal-panel%
+                                 [parent panel]
+                                 [stretchable-width #t]
+                                 [stretchable-height #f]))
+
+    (define selection-message
+      (new message%
+           [label ""]
+           [parent selection-panel]
+           [font (make-object font%
+                   (+ 2 (send normal-control-font get-size))
+                   (send normal-control-font get-family)
+                   'normal
+                   'bold)]
+           [stretchable-width #t]))
+    
+    (define data-list
+      (let ([data-list
+             (new data-list-box%
+                  [label ""]
+                  [parent panel]
+                  [choices '()]
+                  [columns column-names]
+                  [style '(single column-headers)]
+                  [min-height 200]
+                  [callback (lambda (data-list event)
+                              (let ([event-type (send event get-event-type)])
+                                (cond
+                                  ((equal? event-type 'list-box)
+                                   (let ([new-stop (selected-stop-from-list)])
+                                     (when (not (equal? selected-stop new-stop))
+                                       (set! selected-stop new-stop)
+                                       (set-selection-message selected-stop)
+                                       (when callback (callback selection-id new-stop)))))
+                                  ((equal? event-type 'list-box-column)
+                                   (set! list-sorting (send event get-column))))))])])
+        (send data-list set-column-widths '(300 200 400) 100 100)
+        #;(populate-list data-list)
+        data-list))
+
+    (define filter-panel (new horizontal-panel%
+                              [parent panel]
+                              [stretchable-height #f]
+                              [border 10]))
+
+    (define filter-checkbox (new check-box%
+                                 [label "Name filter"]
+                                 [parent filter-panel]
+                                 [value #t]))
+
+    (define filter-textfield (new text-field%
+                                  [label ""]
+                                  [parent filter-panel]))
+
+    (define lon-panel (new vertical-panel%
+                           [parent panel]
+                           [stretchable-height #f]
+                           [border 10]))
+    
+    (define lon-checkbox (new check-box%
+                              [label "Longitude filter"]
+                              [parent (new horizontal-panel% [parent lon-panel])]))
+           
+    (define min-lon-slider
+      (new slider%
+           [label (send slider-converter min-lon-label slider-min)]
+           [parent lon-panel]
+           [min-value slider-min]
+           [max-value slider-max]
+           [init-value slider-min]
+           [style '(plain horizontal)]
+           [callback (lambda (slider event)
+                       (let ([max-lon (send max-lon-slider get-value)])
+                         (when (> (send slider get-value) max-lon)
+                           (send slider set-value max-lon)))
+                       (send slider set-label
+                             (send slider-converter min-lon-label (send slider get-value))))]))
+
+    (define max-lon-slider
+      (new slider%
+           [label (send slider-converter max-lon-label slider-max)]
+           [parent lon-panel]
+           [min-value slider-min]
+           [max-value slider-max]
+           [init-value slider-max]
+           [style '(plain horizontal)]
+           [callback (lambda (slider event)
+                       (let ([min-lon (send min-lon-slider get-value)])
+                         (when (< (send slider get-value) min-lon)
+                           (send slider set-value min-lon)))
+                       (send slider set-label
+                             (send slider-converter max-lon-label (send slider get-value))))]))
+
+    (define lat-panel (new vertical-panel%
+                           [parent panel]
+                           [stretchable-height #f]
+                           [border 10]))
+
+    (define lat-checkbox (new check-box%
+                              [label "Latitude filter"]
+                              [parent (new horizontal-panel% [parent lat-panel])]))
+
+    (define min-lat-slider
+      (new slider%
+           [label (send slider-converter min-lat-label slider-min)]
+           [parent lat-panel]
+           [min-value slider-min]
+           [max-value slider-max]
+           [init-value slider-min]
+           [style '(plain horizontal)]
+           [callback (lambda (slider event)
+                       (let ([max-lat (send max-lat-slider get-value)])
+                         (when (> (send slider get-value) max-lat)
+                           (send slider set-value max-lat)))
+                       (send slider set-label
+                             (send slider-converter min-lat-label (send slider get-value))))]))
+
+    (define max-lat-slider
+      (new slider%
+           [label (send slider-converter max-lat-label slider-max)]
+           [parent lat-panel]
+           [min-value slider-min]
+           [max-value slider-max]
+           [init-value slider-max]
+           [style '(plain horizontal)]
+           [callback (lambda (slider event)
+                       (let ([min-lat (send min-lat-slider get-value)])
+                         (when (< (send slider get-value) min-lat)
+                           (send slider set-value min-lat)))
+                       (send slider set-label
+                             (send slider-converter max-lat-label (send slider get-value))))]))
+    
+    ;;; private methods
+
+    (define (set-selection-message stop)
+      (let ([new-label (if stop
+                           (format "~a (~a / ~a)"
+                                   (name stop)
+                                   (format-range (lon-range stop))
+                                   (format-range (lat-range stop)))
+                           "no stop selected")])
+        (send selection-message set-label new-label)))
+
+    (define (filter-name?) (send filter-checkbox get-value))
+    (define (filter-lon?) (send lon-checkbox get-value))
+    (define (filter-lat?) (send lat-checkbox get-value))
+    
+    (define (list-layout-from-controls)
+      (list-layout
+       (if (filter-name?)
+           (send filter-textfield get-value)
+           "")
+       (if (filter-lon?)
+           (send slider-converter slider->lon (send min-lon-slider get-value))
+           min-lon)
+       (if (filter-lon?)
+           (send slider-converter slider->lon (send max-lon-slider get-value))
+           max-lon)
+       (if (filter-lat?)
+           (send slider-converter slider->lat (send min-lat-slider get-value))
+           min-lat)
+       (if (filter-lat?)
+           (send slider-converter slider->lat (send max-lat-slider get-value))
+           max-lat)
+       list-sorting))
+    
+    (define (populate-list)
+      (let ([old-layout (send data-list get-meta-data)]
+            [new-layout (list-layout-from-controls)])
+        (when (not (equal? old-layout new-layout))
+          (send data-list set-meta-data new-layout)
+          (set-data (~> (filter-stops stops new-layout)
+                        (sort-stops (list-layout-sorting new-layout))))))) 
+
+    (define (selected-stop-from-list)
+      (if-let [selected-index (send data-list get-selection)]
+              (send data-list get-data selected-index)
+              #f))
+    
+    (define (set-data stops)
+      (send/apply data-list set (let-values ([(names lons lats)
+                                              (for/lists (names lons lats)
+                                                ([stop stops])
+                                                (values (~a (name stop))
+                                                        (~a (format-range (lon-range stop)))
+                                                        (~a (format-range (lat-range stop)))))])
+                                  (list names lons lats)))
+      ; associate stop structure as data
+      (for ([index (in-naturals 0)]
+            [stop stops])
+        (send data-list set-data index stop)))
+
+    (define (filter-stops stops list-layout)
+      (filter
+       (lambda (stop)
+         (and (filter-expr-match?
+               (format "(?i:~a)" (list-layout-filter-expr list-layout))
+               (stop-name stop))
+              (>= (stop-lon stop) (list-layout-min-lon list-layout))
+              (<= (stop-lon stop) (list-layout-max-lon list-layout))
+              (>= (stop-lat stop) (list-layout-min-lat list-layout))
+              (<= (stop-lat stop) (list-layout-max-lat list-layout))))
+       stops))
+
+    (define (sort-stops stops sorting-index)
+      (let ([accessor (eval (second (vector-ref column-mappings sorting-index))
+                            (module->namespace "data-defs.rkt"))])
+        (sort stops (lambda (stop1 stop2)
+                      (let ([value1 (accessor stop1)]
+                            [value2 (accessor stop2)])
+                        (if (string? value1)
+                            (string<? value1 value2)
+                            (< value1 value2)))))))
+
+    (define update-timer
+      (new timer%
+           [interval 100]
+           [notify-callback (lambda ()
+                              (populate-list))]))
+
+    ;;; public methods
+
+    (define/public (get-selected-stop)
+      selected-stop)
+    ))
+
+(provide compound-stop-selector%)
--- sub2/data-defs.rkt
+++ sub2/data-defs.rkt
@@ -8,14 +8,20 @@
 (require (prefix-in hav: "haversine.rkt"))
 
 (define-generics compoundable
-  (constituents compoundable))
+  (constituents compoundable)
+  (name compoundable)
+  (lon-range compoundable)
+  (lat-range compoundable))
 
 ;;; Stop
 
 (struct stop (id lon lat name alt-name)
   #:transparent
   #:methods gen:compoundable
-  [(define (constituents stop) (list stop))]
+  [(define (constituents stop) (list stop))
+   (define (name stop) (stop-name stop))
+   (define (lon-range stop) (stop-lon stop))
+   (define (lat-range stop) (stop-lat stop))]
   #:methods gen:custom-write
   [(define write-proc
      (make-constructor-style-printer
@@ -47,22 +53,47 @@
 (define (min-lat stops) (process-filter stops min stop-lat default-min))
 (define (max-lat stops) (process-filter stops max stop-lat default-max))
 
-;;; Compound
+;;; Compound Stop
 
 (struct compound-stop (stops)
   #:transparent
+
   #:methods gen:compoundable
-  [(define (constituents compound) (compound-stop-stops compound))]
+  [(define (constituents compound) (compound-stop-stops compound))
+
+   (define (name compound) (stop-name (set-first (constituents compound))))
+
+   (define (lon-range compound)
+     (let ([stops (set->list (constituents compound))])
+       (cons (min-lon stops) (max-lon stops))))
+
+   (define (lat-range compound)
+     (let ([stops (set->list (constituents compound))])
+       (cons (min-lat stops) (max-lat stops))))]
+
   #:methods gen:custom-write
   [(define write-proc
      (make-constructor-style-printer
       (lambda (c) 'compound-stop)
-      (lambda (c) (list (compound-stop-stops c)))))])
+      (lambda (c) (list
+                   "name" (name c)
+                   "lon-range" (format-range (lon-range c))
+                   "lat-range" (format-range (lat-range c))
+                   (compound-stop-stops c)))))])
+
+(define (format-range range)
+  (match-let ([(cons min max) range])
+    (format "~a - ~a"
+            (coord->string min)
+            (coord->string max))))
+
+(define (wrap-stops-as-compounds stops)
+  (map (lambda (stop) (compound-stop (set stop))) stops))
 
 (define (compound-stops-by-name stops)
   (~>> stops
        (group-by (lambda (stop) (stop-name stop)))
-       (map (lambda (stops) (compound-stop stops)))))
+       (map (lambda (stops) (compound-stop (list->set stops))))))
 
 ;;; Route
 
