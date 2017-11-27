0a526da4f014a37884c703b12a1526042b030c34
7enderhead <7enderhead@gmail.com>
2017-11-27 15:52:42 +0100

    data-provider interface documentation

---
+++ sub2/create_doc.bat
@@ -0,0 +1,1 @@
+scribble.exe --style doc\manual-style.css ++style doc\manual-racket.css ++style doc\manual-fonts.css ++xref-in setup/xref load-collections-xref  --redirect-main "https://docs.racket-lang.org" --dest doc data-provider.rkt  
--- sub2/data-provider.rkt
+++ sub2/data-provider.rkt
@@ -1,15 +1,58 @@
-#lang racket
+#lang scribble/lp2
 
-(require racket/class/iop)
+@title{Interface data-provider<%>}
 
-(define-interface data-provider<%>
-  ()
-  (stops
-   stops-by-id
-   routes
-   routes-for-stop
-   route-exists?
-   insert-route
-   add-callback))
+@section{Documentation}
 
-(provide data-provider<%>)
+@definterface[data-provider<%> ()]{
+
+ The @racket[data-provider<%>] interface provides access to a data layer.
+
+ @defmethod[(stops) (listof stop?)]{
+  Returns all stops. Since these are never changed, caching can happen.
+ }
+
+ @defmethod[(stops-by-id) (hash/c number? stop?)]{
+  Returns all stops in a hashset with @racket[stop-id] as their key. Caching may happen.
+ }
+
+ @defmethod[(routes) (listof route?)]{
+  Returns all routes. Since routes may change, use @racket[add-callback] to register for changes.
+ }
+
+ @defmethod[(routes-for-stop [stop stop?]) (listof route?)]{
+  Returns all routes that use the given stop.
+ }
+
+ @defmethod[(route-exists? [route route?]) boolean?]{
+  Takes a @racket[route] structure and checks whether it already exists in the data layer.
+  @racket[route]'s @racket[route-id] is ignored.
+ }
+
+ @defmethod[(insert-route [stop stop?] [stop-ids (listof number?)]) void]{
+  Inserts a new route.
+
+  @racket[stop-ids] is optional and defaults to @racket[null]. If given, all stops referenced in @racket[stop-ids] are linked with the new route.
+ }
+
+ @defmethod[(add-callback [callback (-> void void)]) void]{
+  Adds a callback to the data layer, which is invoked if the routes have changed.
+ }
+ 
+}
+@section{Implementation}
+
+@chunk[<data-provider>
+       (require racket/class)
+       
+       (define data-provider<%>
+         (interface ()
+           stops
+           stops-by-id
+           routes
+           routes-for-stop
+           route-exists?
+           insert-route
+           add-callback))
+       
+       (provide data-provider<%>)]
