b39a927f725e409a601fb58ce073d33de368b778
gisi <7enderhead@gmail.com>
2017-12-26 10:04:02 +0100

    stop selection retention

--- sub3/web.rkt
+++ sub3/web.rkt
@@ -32,7 +32,7 @@
          [in-stop1 #f]
          [in-use-name-filter1? #t]
          [in-name-filter1 ""])
-  (printf "stop-list-formlet in-stop1: ~v, in-use-name-filter1?: ~v, in-name-filter1: ~v~n" in-stop1 in-use-name-filter1? in-name-filter1)
+  (printf "** formlet in-stop1: ~v, in-use-name-filter1?: ~v, in-name-filter1: ~v~n" in-stop1 in-use-name-filter1? in-name-filter1)
   (formlet
    (#%#
     (h2 ,(if in-stop1
@@ -44,11 +44,16 @@
                                             (list 'size
                                                   (->string (info 'web-stop-list-size))))
                               #:display (lambda (stop)
-                                          (stop-name stop))) . => . stop1})
+                                          (stop-name stop))
+                              #:selected? (lambda (stop)
+                                            (equal? stop in-stop1))) . => . stop1})
     (div ,{(cross (pure (lambda (x) (and x #t)))
                   (checkbox "" in-use-name-filter1?)) . => . use-name-filter1?}
          "Name filter "
-         ,{(to-string (text-input #:value in-name-filter1)) . => . name-filter1})
+         ,{(to-string (default #"" (text-input
+                                    #:value (if in-name-filter1
+                                                in-name-filter1
+                                                "")))) . => . name-filter1})
     (div ,{(input #:type "range") . => . min-lon}))
    (values stop1 use-name-filter1? name-filter1 min-lon)))
 
@@ -56,22 +61,21 @@
   (not (null? (force (request-bindings/raw-promise request)))))
 
 (define (render-stop-info-page request)
-  (printf "request bindings: ~v~n" (force (request-bindings/raw-promise request)))
+  #;(printf "render request bindings: ~v~n" (force (request-bindings/raw-promise request)))
   (define-values (stop1-in use-name-filter1? name-filter1 min-lon)
-    (with-handlers ([(lambda (e) #t) (lambda (e) (values #f #t #f #f))])
+    (with-handlers ([(lambda (e) #t) (lambda (e)
+                                       (printf "!! exception ~v~n" e)
+                                       (values #f #t #f #f))])
       (formlet-process (stop-list-formlet) request)))
   (define stop1 (if (and stop1-in (not (null? stop1-in)))
                     (car stop1-in)
                     #f))
-  (printf "stop1: ~v, use-name-filter?: ~v, name-filter: ~v~n" stop1 use-name-filter1? name-filter1)
+  #;(printf "render stop1: ~v, use-name-filter?: ~v, name-filter: ~v~n" stop1 use-name-filter1? name-filter1)
   (define (response-generator embed/url)
     (response/xexpr
      `(html
        (head (title "Stop Info"))
        (body (h1 "List of Stops")
-             #;(h2 ,(if stop1
-                        (stop-name stop1)
-                        "no stop selected"))
              (form ([action ,(embed/url render-stop-info-page)])
                    ,@(formlet-display (stop-list-formlet stop1
                                                          use-name-filter1?
